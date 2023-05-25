5c401176a0fe7d2de80d4962a3e0fce5b1cb152d
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-25 16:31:24 +0200

    function index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        return [
+            'prova' => 1
+        ];
+    }
 }
