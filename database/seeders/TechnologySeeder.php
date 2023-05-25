7259c0869dfd2733c3f22f30c5a3cfe2ee7652ff
Emanuele Volpes <118191083+emanuelevolpes@users.noreply.github.com>
2023-05-25 15:57:36 +0200

    Initial commit

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['HTML', 'CSS', 'JS', 'PHP', 'Sass'];
+
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        foreach ($technologies as $technology) {
+            
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name);
+            $newTechnology->save();
+        }
+    }
+}
