e4160fa5f04384129a3638f09a04e20ba7b8a109
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-28 17:25:05 +0200

    detail route

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('test', [ProjectController::class, 'index']);
+Route::get('projects', [ProjectController::class, 'index']);
+Route::get('project/{slug}', [ProjectController::class, 'show']);
