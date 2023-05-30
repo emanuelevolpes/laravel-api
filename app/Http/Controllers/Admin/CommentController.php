cdca4f61496e4379a8963b084901d5713a5265c1
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-30 16:33:40 +0200

    function destroy

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function destroy(Comment $comment)
+    {
+
+        $project = $comment->project;
+        $comment->delete();
+
+        return redirect()->route('admin.projects.show', $project);
+    }
 }
