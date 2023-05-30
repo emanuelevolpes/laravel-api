ee6a750d4aa3f853a4fb6e3a7654bb5f1841e5fd
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-30 17:29:02 +0200

    validation

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function store(Request $request){
+
+        $data = $request->validate([
+            'author' => 'nullable|string|max:150',
+            'content' => 'string',
+            'project_id' => 'integer|exists:projects,id'
+        ]);
 
         return $request->all();
     }
