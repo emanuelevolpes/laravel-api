a78db7da76c70859919f9243685e9df82932e75b
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-30 16:24:00 +0200

    comments list in show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,12 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container text-center p-3">
+    <div class="container p-3">
         <h1>{{ $project->title }}</h1>
-        <div><img src="{{ asset('storage/' . $project->image)}}" alt="{{ $project->title }}" style="width: 200px"></div>
+        <hr>
+        <div><img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->title }}" style="width: 200px"></div>
+        <hr>
         @if ($project->type_id)
             <h2>Type: {{ $project->type->name }}</h2>
         @endif
+        <hr>
         <p>{{ $project->description }}</p>
+
+        @if ($project->comments->count())
+            <hr>
+            <div class="comments">
+                <h3 class="mb-3">Comments</h3>
+                <ul>
+                    @foreach ($project->comments as $comment)
+                        <li>
+                            <div>
+                                <h4>{{ $comment->author !== null ? $comment->author : 'Unknown User' }}</h4>
+                                <p>{{ $comment->content }}</p>
+                                <form action="" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                                <hr>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
 @endsection
